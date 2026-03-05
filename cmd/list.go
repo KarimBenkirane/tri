7daa6514f0dc2cf68fe4938b903a1cdd1713bcf6
Karim Benkirane <benkiranemedkarim@gmail.com>
2026-03-05 14:22:43 +0000

    Break out listRun & add description

--- cmd/list.go
+++ cmd/list.go
@@ -14,20 +14,17 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "List the todos",
+	Long:  `Listing the todos`,
+	Run:   listRun,
+}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		items, err := todo.ReadItems(dataFile)
-		if err != nil {
-			log.Printf("%v", err)
-		}
-		fmt.Println(items)
-	},
+func listRun(cmd *cobra.Command, args []string) {
+	items, err := todo.ReadItems(dataFile)
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	fmt.Println(items)
 }
 
 func init() {
