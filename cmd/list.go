f6de3624cda7627510301f55af4b235dadbc13c5
Karim Benkirane <benkiranemedkarim@gmail.com>
2026-03-06 11:37:46 +0000

    Change Less function logic for sorting

--- cmd/list.go
+++ cmd/list.go
@@ -30,7 +30,7 @@ func listRun(cmd *cobra.Command, args []string) {
 	sort.Sort(todo.ByPri(items))
 	w := tabwriter.NewWriter(os.Stdout, 3, 0, 1, ' ', 0)
 	for _, i := range items {
-		fmt.Fprintln(w, i.Label()+"\t"+i.PrettyP()+"\t"+i.Text+"\t")
+		fmt.Fprintln(w, i.Label()+"\t"+i.PrettyDone()+"\t"+i.PrettyP()+"\t"+i.Text+"\t")
 	}
 	w.Flush()
 }
--- todo/todo.go
+++ todo/todo.go
@@ -18,10 +18,13 @@ type ByPri []Item
 func (s ByPri) Len() int      { return len(s) }
 func (s ByPri) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
 func (s ByPri) Less(i, j int) bool {
-	if s[i].Priority == s[j].Priority {
-		return s[i].position < s[j].position
+	if s[i].Done != s[j].Done {
+		return s[i].Done
 	}
-	return s[i].Priority < s[j].Priority
+	if s[i].Priority != s[j].Priority {
+		return s[i].Priority < s[j].Priority
+	}
+	return s[i].position < s[j].position
 }
 
 func SaveItems(filename string, items []Item) error {
